7b906acc9d8022aeb41f9bc39600033835e8af93
Breno R. Monteiro <monteiro1breno@gmail.com>
2016-11-24 18:35:18 -0300

    Knex migration

---
+++ 4.1-knex-js-sqlite-postgresql/intro-knex/migrations/20161124183353_adiciona_marca_cachorro.js
@@ -0,0 +1,9 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.alterTable("cachorro", table => {
+    table.string("marcacachorro");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("cachorro", table => dropColumn("marcacachorro"));
+};
